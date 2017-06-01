5294ca54a0fe25073d225b5bedfd4294bf08f2d8
hellosylvee <helloimsylvia@gmail.com>
2017-06-01 16:42:19 -0400

    adding something

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::AuthController < ApplicationController
     account = Account.find_by(username: params[:username])
     if account.present? && account.authenticate(params[:password])
       ## create a JWT token that encodes the account_id and send that back as part of the response...
-      token = JWT.encode({account_id: account.id}, ENV['JWT_SECRET'], 'HS256')
+      token = JWT.encode({account_id: account.id}, 'hellosylvee', 'HS256')
       render json: {
         account: {
           username: account.username
