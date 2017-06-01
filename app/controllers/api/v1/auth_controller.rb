a1fb31e92c55bbc50e1f87b5714f40af4f25a63d
ipc103 <iancandysemail@gmail.com>
2017-06-01 10:21:04 -0400

    update challenge

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -3,6 +3,7 @@ class Api::V1::AuthController < ApplicationController
   def create
     account = Account.find_by(username: params[:username])
     if account.present? && account.authenticate(params[:password])
+      ## create a JWT token that encodes the account_id and send that back as part of the response...
       render json: account
     else
       render json: {error: 'No account or password found'}
